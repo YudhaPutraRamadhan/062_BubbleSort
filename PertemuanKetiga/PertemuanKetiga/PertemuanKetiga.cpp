8d691433fbfe6c5784fce4a8b4e95a0905b48f19
MzSuwawa <yudharamadhan838@gmail.com>
2024-03-08 16:57:32 +0700

    Perbaikan error

--- PertemuanKetiga/PertemuanKetiga/PertemuanKetiga.cpp
+++ PertemuanKetiga/PertemuanKetiga/PertemuanKetiga.cpp
@@ -25,3 +25,20 @@ void input() {		//Prosedur input
 		cin >> arr[i];
 	}
 }
+
+void BubbleSortArray() {
+	int pass = 1;
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1;
+
+	} while (pass <= n - 1);
+}
