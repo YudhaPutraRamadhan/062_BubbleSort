cbe3cf3f84afa227f77aab65d020a5774b24883e
MzSuwawa <yudharamadhan838@gmail.com>
2024-03-08 16:55:12 +0700

    Procedur untuk mengurutkan array dengan metode bubble sort

--- PertemuanKetiga/PertemuanKetiga/PertemuanKetiga.cpp
+++ PertemuanKetiga/PertemuanKetiga/PertemuanKetiga.cpp
@@ -3,3 +3,25 @@
 using namespace std;
 int arr[20];        //Deklarasi variabel global array a dengan ukuran 20
 int n;              // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+
+void input() {		//Prosedur input
+	int d;
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : ";  // output ke layar
+		cin >> n;	// input dari pengguna
+		if (n <= 20) // jika n kurang dari atau sama dengan 20
+			break;  // keluar dari loop
+		else {		// jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan elemen array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << " : ";
+		cin >> arr[i];
+	}
+}
